17ffe6bd7090aec763024ba39de21297701b168d
Komarov Oleg <komarov.om@gmail.com>
2010-02-09 00:49:37 +0300

    fixed issue with attributes + minor refactoring

--- undeclare.pl
+++ undeclare.pl
@@ -5,7 +5,7 @@
 # Motivation: http://www.mail-archive.com/moose@perl.org/msg01220.html
 #
 # Authors and contributors: Komarov Oleg, Kim Vladimir
-# Last update: 2010-01-11
+# Last update: 2010-02-09
 
 use strict;
 use warnings;
@@ -22,60 +22,23 @@ my $Methods = $Doc->find( sub { return $_[ 1 ]->isa( 'PPI::Token::Word' ) && $_[
 if( ref( $Methods ) eq 'ARRAY' ) {
    foreach my $Method ( @$Methods ) {
       $Method->set_content( 'sub' );
-      my $AfterMethodName = $Method->snext_sibling()->snext_sibling();
-      my $CodeBlock;
-      my $InsertBlock = "\n";
-      if( $AfterMethodName->isa( 'PPI::Structure::List' ) ) {
-      # method has a signature
-         $CodeBlock = $AfterMethodName->snext_sibling();
 
-         my $SignatureToken = $AfterMethodName->remove();
-         my $Signature      = Parse::Method::Signatures->signature( $SignatureToken->content() );
-         my @Lexicals = ();
-         push @Lexicals, $Signature->has_invocant ? $Signature->invocant->variable_name : '$self';
-
-         my %DefaultValues = ();
-         if( $Signature->has_positional_params ) {
-            push @Lexicals, map { $_->variable_name } $Signature->positional_params;
-            foreach my $Param ( grep { $_->has_default_value } $Signature->positional_params ) {
-               $DefaultValues{ $Param->variable_name } = $Param->default_value;
-            }
+      my $MethodName = $Method->snext_sibling();
+      my $SignatureToken;
+      my $Sibling = $MethodName->snext_sibling();
+      while( !$Sibling->isa( 'PPI::Structure::Block' ) ) {
+         if( $Sibling->isa( 'PPI::Structure::List' ) ) {
+            $SignatureToken = $Sibling;
          }
-         my $NamedParametersBlock = '';
-         if( $Signature->has_named_params ) {
-            my $MaxVariableNameLength = max( map { length( $_->variable_name ) } $Signature->named_params );
-            push @Lexicals, '%__named_params';
-            $NamedParametersBlock = join( 
-               '', 
-               map { 
-                  "my " . $_->variable_name . ( ' ' x ( $MaxVariableNameLength - length( $_->variable_name ) ) ) 
-                  . " = \$__named_params{ '" . $_->label . "' };\n" 
-               } $Signature->named_params 
-            );
-            foreach my $Param ( grep { $_->has_default_value } $Signature->named_params ) {
-               $DefaultValues{ $Param->variable_name } = $Param->default_value;
-            }
-         }
-
-         my $Vars = join q{,}, @Lexicals;
-         $InsertBlock .= "my (${Vars}) = \@_;\n";
-
-         my $DefaultValuesBlock = '';
-         foreach my $VariableName ( keys %DefaultValues ) {
-            $DefaultValuesBlock .= "$VariableName = $DefaultValues{ $VariableName } if !defined $VariableName;\n"; 
-            # //= operator is not always available
-            # otherwise it would be 
-            # $DefaultValuesBlock .= "$VariableName //= $DefaultValues{ $VariableName };\n"; 
-         }
-         $DefaultValuesBlock = "# Default values\n$DefaultValuesBlock" if $DefaultValuesBlock;
-
-         $InsertBlock .= $NamedParametersBlock;        
-         $InsertBlock .= $DefaultValuesBlock;
-
+         $Sibling = $Sibling->snext_sibling();
       }
-      elsif( $AfterMethodName->isa( 'PPI::Structure::Block' ) ) {
-      # method doesn't have a signature
-         $CodeBlock = $AfterMethodName;
+      my $CodeBlock = $Sibling;
+      my $InsertBlock = "\n";
+      if( $SignatureToken ) {
+         $InsertBlock .= processSignature( $SignatureToken );
+         $SignatureToken->remove();
+      }
+      else {
          $InsertBlock .= "my \$self = shift;\n";
       }
 
@@ -102,4 +65,56 @@ sub prefix {
    my ( $Block, $Prefix ) = @_;
    my @Lines = split /(\n)/, $Block;
    return join( '', map { $_ eq "\n" ? $_ : $Prefix . $_ } @Lines );
+}
+
+
+#-------------------------------------------------------------------------------
+sub processSignature {
+   my $SignatureToken = shift;
+
+   my $Signature      = Parse::Method::Signatures->signature( $SignatureToken->content() );
+   my @Lexicals = ();
+   push @Lexicals, $Signature->has_invocant ? $Signature->invocant->variable_name : '$self';
+
+   my $Output = '';
+
+   my %DefaultValues = ();
+   if( $Signature->has_positional_params ) {
+      push @Lexicals, map { $_->variable_name } $Signature->positional_params;
+      foreach my $Param ( grep { $_->has_default_value } $Signature->positional_params ) {
+         $DefaultValues{ $Param->variable_name } = $Param->default_value;
+      }
+   }
+   my $NamedParametersBlock = '';
+   if( $Signature->has_named_params ) {
+      my $MaxVariableNameLength = max( map { length( $_->variable_name ) } $Signature->named_params );
+      push @Lexicals, '%__named_params';
+      $NamedParametersBlock = join( 
+         '', 
+         map { 
+            "my " . $_->variable_name . ( ' ' x ( $MaxVariableNameLength - length( $_->variable_name ) ) ) 
+            . " = \$__named_params{ '" . $_->label . "' };\n" 
+         } $Signature->named_params 
+      );
+      foreach my $Param ( grep { $_->has_default_value } $Signature->named_params ) {
+         $DefaultValues{ $Param->variable_name } = $Param->default_value;
+      }
+   }
+
+   my $Vars = join q{,}, @Lexicals;
+   $Output .= "my (${Vars}) = \@_;\n";
+
+   my $DefaultValuesBlock = '';
+   foreach my $VariableName ( keys %DefaultValues ) {
+      $DefaultValuesBlock .= "$VariableName = $DefaultValues{ $VariableName } if !defined $VariableName;\n"; 
+      # //= operator is not always available
+      # otherwise it would be 
+      # $DefaultValuesBlock .= "$VariableName //= $DefaultValues{ $VariableName };\n"; 
+   }
+   $DefaultValuesBlock = "# Default values\n$DefaultValuesBlock" if $DefaultValuesBlock;
+
+   $Output .= $NamedParametersBlock;        
+   $Output .= $DefaultValuesBlock;
+
+   return $Output;
 }
